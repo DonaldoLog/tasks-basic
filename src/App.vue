454da1d177110decad447cda6429796abde9348c
Donaldo Lopez <donaldolog@gmail.com>
2018-12-27 19:15:28 -0600

    formulario basico

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  #app
+    .container
+      img.logo(src="./assets/logo-platzi.png")
+      h1.titulo Registro estación Platzi
+      label.label Nombre
+      input(type="text")
+      label.label Apellido
+      input(type="text")
+      label.label Cargo
+      input(type="text")
+      br
+      button(type="submit") ENVIAR
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola mundo'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
