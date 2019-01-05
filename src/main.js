64c05085bcfebc069ffabfe652b147cdd2182ed0
Donaldo Lopez <donaldolog@gmail.com>
2019-01-04 20:07:11 -0600

    page movie

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faFire, faHeart, faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome, faFire, faHeart, faCog)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
