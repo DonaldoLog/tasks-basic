db315a5c5765f0f023b05e2f1cc46007a55a3c03
Donaldo Lopez <donaldolog@gmail.com>
2019-01-20 13:45:14 -0600

    vue router

---
+++ src/components/Search.vue
@@ -0,0 +1,101 @@
+<template lang="pug">
+  main
+    ap-notification(v-show="showNotification", :tipo="tipo.clase")
+      p(slot="body", v-show="tipo.clase == 'is-danger'") Sin resultados
+      p(slot="body", v-show="tipo.clase == 'is-success'") {{ searchMessage }}
+
+    ap-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.navbar
+        .container
+          input.input.is-large(type="text",
+            placeholder="Buscar canciones...",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+      .container
+        div(v-if="tracks.length > 0")
+          .columns.is-multiline
+            .column.is-one-quarter(v-for="(track, index) in tracks" :key="index")
+              ap-track(
+                :class="{ 'is-active': track.id === selectedTrack }"
+                :track="track",
+                @select="setSelectedTrack"
+              )
+        div(v-else)
+          p Sin resultados
+</template>
+
+<script>
+import trackService from '@/services/track'
+import ApTrack from '@/components/Track.vue'
+
+import ApNotification from '@/components/shared/Notification.vue'
+import ApLoader from '@/components/shared/Loader.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ApTrack, ApLoader, ApNotification
+  },
+  data () {
+    return {
+      searchQuery: '',
+      tracks: [],
+      isLoading: false,
+      selectedTrack: '',
+      showNotification: false,
+      tipo: {
+        clase: '',
+        texto: ''
+      }
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
+  methods: {
+    search () {
+      this.isLoading = true
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.showNotification = true
+          if (res.tracks.total === 0) {
+            this.tipo.clase = 'is-danger'
+          } else {
+            this.tipo.clase = 'is-success'
+          }
+          this.tracks = res.tracks.items
+          this.isLoading = false
+        })
+      console.log(this.searchQuery)
+    },
+    setSelectedTrack (id) {
+      this.selectedTrack = id
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .results {
+    margin-top: 50px;
+  }
+  .is-active {
+    border: 3px #23d160 solid;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome, faFire, faHeart, faCog } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import routes from '@/routes'
 
 import EventeBus from '@/plugins/event-bus'
 
@@ -10,11 +13,16 @@ library.add(faHome, faFire, faHeart, faCog)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
+Vue.use(VueRouter)
+
 Vue.use(EventeBus)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({ routes })
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
